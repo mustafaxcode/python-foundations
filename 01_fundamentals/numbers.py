8513f6813d5cf4108aa3339b0aad61ffabb88de1
Muhammad Mustafa Kamran <mustafaxkamran@gmail.com>
2026-08-07 20:47:49 +0500

    Add numbers and type-casting practice

---
+++ 01_fundamentals/numbers.py
@@ -0,0 +1,21 @@
+"""
+Topic: Numbers & Type Casting
+"""
+
+# -----------------------------------
+# Exercise
+# -----------------------------------
+
+"""
+Take a string "25", cast it to an integer, add 5, print the result.
+
+"""
+
+# -----------------------------------
+# My Solution
+# -----------------------------------
+
+x = "25"
+y = int(x)
+result = y + 5
+print(result)
