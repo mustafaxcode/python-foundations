ecdbb2d6e5d09ad733cfa563e830ab39a79b03fa
Muhammad Mustafa Kamran <mustafaxkamran@gmail.com>
2026-08-07 20:34:24 +0500

    Add variables and data types practice

---
+++ 01_fundamentals/variables.py
@@ -0,0 +1,27 @@
+"""
+Topic: Variables & Data Types
+"""
+
+# -----------------------------------
+# Exercise
+# -----------------------------------
+
+"""
+Create variables of each type (int, float, string, boolean) and
+print their types using type().
+
+"""
+
+# -----------------------------------
+# My Solution
+# -----------------------------------
+
+a = 2
+b = 3.0
+c = "Hello"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
