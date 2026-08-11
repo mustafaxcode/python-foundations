dde5c8b9938b05b5c48565d867e2acbd52ac83f8
Muhammad Mustafa Kamran <mustafaxkamran@gmail.com>
2026-08-11 18:59:59 +0500

    Add operators practice

---
+++ 01_fundamentals/operators.py
@@ -0,0 +1,26 @@
+"""
+Topic: Operators
+"""
+
+# -----------------------------------
+# Exercise
+# -----------------------------------
+
+"""
+Write one line each using arithmetic, comparison, and logical
+operators together.
+
+"""
+
+# -----------------------------------
+# My Solution
+# -----------------------------------
+
+one = (10 + 40 > 20 / 2) or (20 % 2 == 35 - 15)
+print(one)
+
+two = not(16 * 2 + 3 - 15 != 40 / 2) and ( 10 ** 2 >= 25 % 2 * 35 )
+print(two)
+
+three = (24 / 12 == 2) and not(100 * 0 <= 67)
+print(three)
