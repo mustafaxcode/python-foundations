e1079bbc72d3476a34226819ee0eff8a95397abe
Muhammad Mustafa Kamran <mustafaxkamran@gmail.com>
2026-09-09 20:53:35 +0500

    Add set operations practice

---
+++ 02_data_structures/sets.py
@@ -0,0 +1,23 @@
+"""
+Topic: Sets
+"""
+
+# -----------------------------------
+# Exercise
+# -----------------------------------
+
+"""
+Create two sets of numbers with overlap, find their intersection and
+union.
+
+"""
+
+# -----------------------------------
+# My Solution
+# -----------------------------------
+
+x = {2,3,5,4,6}
+y = {1,7,3,5,8}
+
+print(x.intersection(y))
+print(x.union(y))
