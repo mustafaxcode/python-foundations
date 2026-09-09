b3ccdf2b55c653058af42b1c1d70ffec6e5167a2
Muhammad Mustafa Kamran <mustafaxkamran@gmail.com>
2026-09-09 20:47:21 +0500

    Add list operations practice

---
+++ 02_data_structures/lists.py
@@ -0,0 +1,30 @@
+"""
+Topic: Lists
+"""
+
+# -----------------------------------
+# Exercise
+# -----------------------------------
+
+"""
+Build a list of 5 numbers, sort it, reverse it, remove one item, add a new
+one
+
+"""
+
+# -----------------------------------
+# My Solution
+# -----------------------------------
+
+x = [2,4,3,1,5]
+x.sort()
+print(x)
+
+x.reverse()
+print(x)
+
+x.remove(3)
+print(x)
+
+x.append(6)
+print(x)
