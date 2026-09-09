87bae6f17ce087ef55aa34c17f102c74041307ba
Muhammad Mustafa Kamran <mustafaxkamran@gmail.com>
2026-09-09 20:50:34 +0500

    Add tuple practice

---
+++ 02_data_structures/tuples.py
@@ -0,0 +1,20 @@
+"""
+Topic: Tuples
+"""
+
+# -----------------------------------
+# Exercise
+# -----------------------------------
+
+"""
+Create a tuple representing coordinates (x, y), and explain in a comment
+why it shouldn't be a list.
+
+"""
+
+# -----------------------------------
+# My Solution
+# -----------------------------------
+
+# We do not use a list because lists are mutable meaning their values
+# can be changed while a tuple keeps the coordinates fixed.
